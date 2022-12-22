13d26b34bfe08ee137d00b52bb5d0be0e68f1776
casfalking <thecaasantos@gmail.com>
2022-12-22 16:44:31 -0300

    feat: build application

--- components/Task.js
+++ components/Task.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Task = (props) => {
 
     return (
-        <View style= { styles.item }>
+        <View style={ styles.item }>
             <View style={ styles.itemLeft }>
                 <View style={ styles.square }></View>
                 <Text styles={ styles.itemText }>{ props.text }</Text>
@@ -15,33 +15,35 @@ const Task = (props) => {
 }
 
 const styles = StyleSheet.create({
-    itemLeft: {
+    item: {
         backgroundColor: '#FFF',
         padding: 15,
         marginBottom: 20,
+        borderRadius: 10,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
     },
 
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+    },
+
     square: {
         width: 24,
         height: 24,
-        backgroundColor: 'black',
-        opacity: -.4,
+        backgroundColor: 'red',
+        opacity: 0.8,
         borderRadius: 5,
-        marginLeft: 15,
+        marginHorizontal: 15,
 
     },
 
     itemText: {
         maxWidth: '80%',
 
-    },
-    
-    item: {
-       
-        
     },
 
     circular: {
@@ -50,7 +52,7 @@ const styles = StyleSheet.create({
         borderColor: '#55BCF6',
         borderWidth: 2,
         borderRadius: 5,
-    }
+    },
 
 })
 
